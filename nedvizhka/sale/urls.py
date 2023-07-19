14d8826f5d827df88a60b84bcaa9a3edab02f1e1
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-07-19 15:35:11 +0300

    Создание страницы create для выбора нужной модели

--- nedvizhka/sale/urls.py
+++ nedvizhka/sale/urls.py
@@ -4,8 +4,8 @@ from .views import *
 urlpatterns = [
     path('flat/', FlatListView.as_view(template_name="flat.html"), name='flat'),
     path('flat/<int:pk>', FlatDetailView.as_view(), name='flat'),
-    path('flat/create/', FlatCreateView.as_view()),
+    path('flat/create/', FlatCreateView.as_view(), name="createflat"),
     path('house/', HouseListView.as_view(template_name="house.html"), name='house'),
     path('house/<int:pk>', HouseDetailView.as_view(), name='house'),
-    path('house/create/', HouseCreateView.as_view())
+    path('house/create/', HouseCreateView.as_view(), name="createhouse")
 ]
--- nedvizhka/user_profile/urls.py
+++ nedvizhka/user_profile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import *
 from . import views
 
 urlpatterns = [
     path('', views.ProfileView.as_view(), name='profile'),
     path('edit_profile/', views.EditProfileView.as_view(), name='edit_profile'),
+    path('create/', TemplateView.as_view(template_name="create.html"), name='create'),
 ]
