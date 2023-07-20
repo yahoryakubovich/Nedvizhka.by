403554dfbf55deb55b1d58cf3cd1b06c34ba6ce6
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-07-20 04:19:22 +0300

    Views и templates for sale app, доработать detail view

--- nedvizhka/sale/views.py
+++ nedvizhka/sale/views.py
@@ -1,13 +1,11 @@
 from django.views.generic import *
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from .models import *
 from .forms import *
 
 
 class FlatListView(ListView):
     model = Flat
-    paginate_by = 20
 
     def get_queryset(self):
         return super().get_queryset().filter(is_moderated=True)
@@ -15,6 +13,48 @@ class FlatListView(ListView):
 
 class HouseListView(ListView):
     model = House
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class GarageListView(ListView):
+    model = Garage
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class ParkingListView(ListView):
+    model = Parking
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class WarehouseListView(ListView):
+    model = Warehouse
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class OfficeListView(ListView):
+    model = Office
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class TradeListView(ListView):
+    model = Trade
+
+    def get_queryset(self):
+        return super().get_queryset().filter(is_moderated=True)
+
+
+class IndustrialListView(ListView):
+    model = Industrial
 
     def get_queryset(self):
         return super().get_queryset().filter(is_moderated=True)
@@ -40,19 +80,65 @@ class HouseDetailView(DetailView):
         return self.render_to_response(context)
 
 
-# class FlatCreateView(LoginRequiredMixin, CreateView):
-#     model = Flat
-#     fields = '__all__'
-#     template_name = 'flatcreateview.html'
-#     login_url = ('account_login')
-#     success_url = reverse_lazy('flat')
-#
-#     def form_valid(self, form):
-#         form.save()
-#         return super(FlatCreateView, self).form_valid(form)
-#
-#     def form_invalid(self, form):
-#         return super(FlatCreateView, self).form_valid(form)
+class GarageDetailView(DetailView):
+    model = Garage
+    template_name = "garagedetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class ParkingDetailView(DetailView):
+    model = Parking
+    template_name = "parkingdetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class WarehouseDetailView(DetailView):
+    model = Warehouse
+    template_name = "warehousedetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class OfficeDetailView(DetailView):
+    model = Office
+    template_name = "officedetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class TradeDetailView(DetailView):
+    model = Parking
+    template_name = "tradedetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class IndustrialDetailView(DetailView):
+    model = Parking
+    template_name = "industrialdetailview.html"
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
 
 class FlatCreateView(LoginRequiredMixin, CreateView):
     model = Flat
