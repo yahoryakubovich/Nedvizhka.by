5ab18881c552e514fa5ddf71e1c2f5cc9d2debaa
yahoryakubovich <officialegoryakubovich@gmail.com>
2023-08-09 23:42:56 +0300

    Исправление API

--- nedvizhka/api/serializers.py
+++ nedvizhka/api/serializers.py
@@ -5,14 +5,14 @@ from sale.models import *
 class FlatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flat
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities', 'condition', 'renovation',
+        fields = ['id', 'description', 'total_area', 'address', 'facilities', 'condition', 'renovation',
                   'price', 'number_of_rooms', 'image', 'floor_number', 'living_area', 'kitchen', 'year_of_construction']
 
 
 class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities', 'condition', 'renovation',
+        fields = ['id',  'description', 'total_area', 'address', 'facilities', 'condition', 'renovation',
                   'price', 'category', 'image', 'number_of_rooms', 'number_of_floors', 'living_area',
                   'year_of_construction']
 
@@ -20,41 +20,41 @@ class HouseSerializer(serializers.ModelSerializer):
 class GarageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Garage
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities', ' quantity_parking_spaces',
+        fields = ['id',  'description', 'total_area', 'address', 'facilities', ' quantity_parking_spaces',
                   'heating', 'price', 'image']
 
 
 class ParkingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parking
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities', ' quantity_parking_spaces',
+        fields = ['id', 'description', 'total_area', 'address', 'facilities', ' quantity_parking_spaces',
                   'price', 'image']
 
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities',
+        fields = ['id','description', 'total_area', 'address', 'facilities',
                   'number_of_separate_premises', 'price', 'image']
 
 
 class OfficeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Office
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities',
+        fields = ['id', 'description', 'total_area', 'address', 'facilities',
                   'condition', 'floor_number', 'price', 'image']
 
 
 class TradeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trade
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities',
+        fields = ['id', 'description', 'total_area', 'address', 'facilities',
                   'location', 'price', 'image']
 
 
 class IndustrialSerializer(serializers.ModelSerializer):
     class Meta:
         model = Industrial
-        fields = ['id', 'title', 'description', 'total_area', 'address', 'facilities',
+        fields = ['id', 'description', 'total_area', 'address', 'facilities',
                   'number_of_separate_premises',
                   'price', 'image']
